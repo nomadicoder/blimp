7876df1353f1c368910cd2d3e654f3e6e9e3b0ac
Steven Ng <steven.ng@temple.edu>
2017-02-24 14:21:52 -0500

    Upload datafile. Stored in cache

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,6 @@
+require 'carrierwave/orm/activerecord'
+
 class Document < ApplicationRecord
-  validates :filename, :id_field, presence: true
+  #validates :filename, :id_field, presence: true
+  mount_uploader :datafile, DatafileUploader
 end
