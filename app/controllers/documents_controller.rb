a90dc29345c48071b4305ecac50b63479b3da5ad
Steven Ng <steven.ng@temple.edu>
2017-02-24 15:19:29 -0500

    Permit access to datafile attribute
    - Trust the datafile parameter
    - Display datafile parameter

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -69,6 +69,6 @@ class DocumentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def document_params
-      params.require(:document).permit(:filename, :id_field)
+      params.require(:document).permit(:filename, :id_field, :datafile)
     end
 end
